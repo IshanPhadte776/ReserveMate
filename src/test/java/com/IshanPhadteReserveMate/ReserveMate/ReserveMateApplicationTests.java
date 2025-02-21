47b3f4c411cc27d0e52f02572ae5709687f27ee9
Ishan Phadte <ishanphadte@gmail.com>
2025-02-20 23:35:07 -0500

    wip

---
+++ src/test/java/com/IshanPhadteReserveMate/ReserveMate/ReserveMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IshanPhadteReserveMate.ReserveMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
